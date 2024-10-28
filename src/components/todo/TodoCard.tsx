26a27743a598728f45f354e9aa3765a6c7117910
Sawom <asawom250@gmail.com>
2024-10-29 02:46:41 +0600

    updated

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -6,7 +6,7 @@ const TodoCard = () => {
         <div className="bg-white rounded-md flex justify-between items-center p-3">
           <input type="checkbox" name="" id="" />
           <p>Todo Title</p>
-          <p>Time</p>
+          {/* <p>Time</p> */}
           <p>Description</p>
 
           <div className="space-x-5">
--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -1,13 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
+// todo er task gular type define korlam
+type TTodo ={
+    title: string;
+    description: string;
+    isCompleted?: boolean;
+}
+
+type TinitialState ={
+    todos: TTodo[];
+}
+
+const initialState : TinitialState = {
     todos: [],
 };
 
 const todoSlice = createSlice({
     name: 'todo',
     initialState,
-    reducers: {}, 
+    reducers: {
+        addTodo: (state, action: PayloadAction<TTodo> )=>{
+            state.todos.push({...action.payload, isCompleted: false}) // task push korlam
+        }
+    }, 
 })
 
+// reducer gula shb export kore dite hobe
+export const {addTodo} = todoSlice.actions;
 export default todoSlice.reducer;
