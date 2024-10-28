9c7abf383faa3fa4e50b6ebe6f24f6a1fa89025c
Sawom <asawom250@gmail.com>
2024-10-28 16:02:20 +0600

    update todo container

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const todoCard = () => {
+const TodoCard = () => {
     return (
-        <div>
-            
+        <div className="bg-white rounded-md flex justify-between items-center p-3">
+          <input type="checkbox" name="" id="" />
+          <p>Todo Title</p>
+          <p>Time</p>
+          <p>Description</p>
+
+          <div className="space-x-5">
+            <button>del</button>
+            <button>edit</button>
+          </div>
         </div>
     );
 };
 
-export default todoCard;
+export default TodoCard;
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,27 +1,24 @@
-import React from 'react';
+import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
-    return (
-        <div>
-            <div>
-                <button>Add todo</button>
-                <button>Filter</button>
-            </div>
-            <div className='bg-red-500 w-full h-[500px] p-5 rounded-xl'>
-                <div className='bg-white' >
-                    <input type="checkbox" name="" id="" />
-                    <p>Todo Title</p>
-                    <p>Time</p>
-                    <p>Description</p>
+  return (
+    <div>
+      <div>
+        <button>Add todo</button>
+        <button>Filter</button>
+      </div>
 
-                    <div>
-                        <button>del</button>
-                        <button>edit</button>
-                    </div>
-                </div>
-            </div>
+      <div className="bg-red-500 w-full h-full p-5 rounded-xl space-y-5">
+        <div className="bg-white font-bold text-xl p-3 flex justify-center items-center" >
+            <p> There is no task pending</p>
         </div>
-    );
+        
+        <TodoCard></TodoCard>
+        <TodoCard></TodoCard>
+        <TodoCard></TodoCard>
+      </div>
+    </div>
+  );
 };
 
 export default TodoContainer;
